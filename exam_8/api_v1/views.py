efe48a4a58de70e9344857e745d9b92bc895f30b
belek <belektolubaev@gmail.com>
2019-03-09 17:50:39 +0600

    Создание вьюшек

--- exam_8/api_v1/views.py
+++ exam_8/api_v1/views.py
@@ -2,7 +2,9 @@ from webapp.models import Task
 from rest_framework import viewsets
 from api_v1.serializers import TaskSerializer
 
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+class NoAuthModelViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+
+class TaskViewSet(NoAuthModelViewSet):
+    queryset = Task.objects.all().order_by('status', '-due_date')
     serializer_class = TaskSerializer
-    
