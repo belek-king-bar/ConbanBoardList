6ef94f1f4173d2731e02a8321e86685bb4738bbe
belek <belektolubaev@gmail.com>
2019-03-09 11:38:49 +0600

    Создание модели и регистрация в админке

--- exam_8/webapp/models.py
+++ exam_8/webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('turn', 'Очередь'),
+        ('in work', 'Выкуплено'),
+        ('done', 'Отмена')
+    ]
+
+    summary = models.TextField(max_length=100, verbose_name="Краткое описание")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Полное описание")
+    due_date = models.DateField(verbose_name="Срок выполнение")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='turn')
+    time_planned = models.DecimalField(max_digits=10, decimal_places=1, null=True, blank=True, verbose_name="Планируемое время")
+
+
+    def __str__(self):
+        return "%s %s %s" % (self.summary, self.due_date, self.status)
+
+
