4f5d622f263e89cea2211caf4e57511944ece60b
belek <belektolubaev@gmail.com>
2019-03-09 11:39:06 +0600

    Создание viewsets

--- exam_8/api_v1/views.py
+++ exam_8/api_v1/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from webapp.models import Task
+from rest_framework import viewsets
+from api_v1.serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
